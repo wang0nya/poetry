ee9a8ab9c7c473b01b8ca4dbf2d8741062343bc9
wang0nya <kwangonya@gmail.com>
2018-07-30 14:44:02 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// register service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/Currency-Converter/sw.js').then(registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+// get all poems from API
+fetch('')
+    .then(response => response.json())
+    .then(poems => {
+        for (let poem in poems) {
+
+        }
+    });
+
+// network listener
+window.addEventListener('load', () => {
+    const status = document.getElementById("status");
+
+    function updateOnlineStatus(event) {
+        const condition = navigator.onLine ? "online" : "offline";
+
+        status.className = condition;
+        if (condition === 'offline') {
+            status.innerHTML = `<div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+                                    You've gone ${condition}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>`;
+        } else if (condition === 'online') {
+            // No need for an alert if user is online already. Everything will be working just fine
+            // Remove offline alert
+            status.innerHTML = '';
+        }
+
+    }
+
+    window.addEventListener('online',  updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+});
---
+++ sw.js
@@ -0,0 +1,65 @@
+// add an install event listener to the service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => cache.addAll([
+            '/poetry/',
+            '/poetry/index.html',
+            '/poetry/404.html',
+            '/poetry/css/main.css',
+            '/poetry/css/bootstrap.min.css',
+            '/poetry/css/normalize.css',
+            '/poetry/css/paper-kit.css',
+            'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css',
+            '/poetry/css/paper-kit.css',
+            '/poetry/js/vendor/modernizr-3.6.0.min.js',
+            '/poetry/js/vendor/jquery-3.3.1.min.js',
+            '/poetry/js/jquery-ui-1.12.1.custom.min.js',
+            '/poetry/js/popper.js',
+            '/poetry/js/plugins.js',
+            '/poetry/js/main.js',
+            '/poetry/js/bootstrap.min.js',
+            '/poetry/js/bootstrap-switch.min.js',
+            '/poetry/js/paper-kit.js',
+            // '', TODO: add poetry api 
+            'https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval-iife.min.js'
+        ]))
+    );
+});
+
+// attach a fetch event listener to the service worker,
+// then call the respondWith() method on the event to hijack HTTP responses
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => {
+        // caches.match() always resolves
+        // but in case of success response will have value
+        if (response !== undefined) {
+            return response;
+        } else {
+            return fetch(event.request).then(response => {
+                // response may be used only once
+                // we need to save clone to put one copy in cache
+                // and serve second one
+                let responseClone = response.clone();
+
+                caches.open('v1').then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            }).catch(() => // return Response;
+                new Response('Something went wrong :('));
+        }
+    }));
+});
+
+// activate
+self.addEventListener('activate', event => {
+    const cacheWhitelist = ['v1'];
+
+    event.waitUntil(
+        caches.keys().then(keyList => Promise.all(keyList.map(key => {
+            if (!cacheWhitelist.includes(key)) {
+                return caches.delete(key);
+            }
+        })))
+    );
+});
