ce415e44f65d2e53768b30bb862939acb4f08aec
wang0nya <kwangonya@gmail.com>
2018-08-01 19:31:20 +0300

    register sw

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 // register service worker
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('/poetry/sw.js').then(registration => {
-//             // Registration was successful
-//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//         }, err => {
-//             // registration failed :(
-//             console.log('ServiceWorker registration failed: ', err);
-//         });
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/poetry/sw.js').then(registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
 
 // get all authors from API
 function explore () {
